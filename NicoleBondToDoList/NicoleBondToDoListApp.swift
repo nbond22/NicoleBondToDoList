a731e899e2b6a3b22818079b4fe11b8e3590979b
Nicole Bond <nicbond22@gmail.com>
2025-04-28 17:58:05 -0400

    Initial Commit

---
+++ NicoleBondToDoList/NicoleBondToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NicoleBondToDoListApp.swift
+//  NicoleBondToDoList
+//
+//  Created by Scholar on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct NicoleBondToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
