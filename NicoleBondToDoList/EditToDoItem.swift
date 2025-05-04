7cb2d782ca9ec792ecf2839f51e89451d280bcf1
Nicole Bond <nicbond22@gmail.com>
2025-05-04 11:31:06 -0400

    Commented out the EditToDoItem file to avoid any errors until functionality is added later

--- NicoleBondToDoList/EditToDoItem.swift
+++ NicoleBondToDoList/EditToDoItem.swift
@@ -1,38 +1,38 @@
+////
+////  EditToDoItem.swift
+////  NicoleBondToDoList
+////
+////  Created by Scholar on 5/4/25.
+////
 //
-//  EditToDoItem.swift
-//  NicoleBondToDoList
+//import SwiftUI
 //
-//  Created by Scholar on 5/4/25.
+//struct EditToDoView: View {
+//    @Binding var task: toDoItem
+//    var onSave: () -> Void
 //
-
-import SwiftUI
-
-struct EditToDoView: View {
-    @Binding var task: toDoItem
-    var onSave: () -> Void
-
-    var body: some View {
-        VStack(spacing: 20) {
-            TextField("Task Title", text: $task.title)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-
-            Toggle("Important", isOn: $task.isImportant)
-
-            Button("Save") {
-                onSave()
-            }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(8)
-        }
-        .padding()
-        .background(Color.white)
-        .cornerRadius(12)
-        .shadow(radius: 5)
-    }
-}
-
-#Preview {
-    EditToDoItem()
-}
+//    var body: some View {
+//        VStack(spacing: 20) {
+//            TextField("Task Title", text: $task.title)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//
+//            Toggle("Important", isOn: $task.isImportant)
+//
+//            Button("Save") {
+//                onSave()
+//            }
+//            .padding()
+//            .background(Color.blue)
+//            .foregroundColor(.white)
+//            .cornerRadius(8)
+//        }
+//        .padding()
+//        .background(Color.white)
+//        .cornerRadius(12)
+//        .shadow(radius: 5)
+//    }
+//}
+//
+//#Preview {
+//    EditToDoItem()
+//}
